48a2a9713fcbb6158adaf46eb10134cad9ae2027
Jodie Putrino <j.putrino@f5.com>
2016-11-08 16:50:01 -0700

    fix version number for LWPC

--- docs/conf.py
+++ docs/conf.py
@@ -85,7 +85,7 @@ author = u'F5 Networks'
 
 rst_epilog = """
 .. |lwp_v| replace:: v0.2.0
-.. |lwpc-v| replace:: v0.2.1
+.. |lwpc-v| replace:: v0.2.0
 .. |csi_k-v| replace:: v0.1.0
 .. |csi_k_v| replace:: |csi_k-v|
 .. |csi_m-v| replace:: v0.1.4
