a7a597f9056d41a034c6e84d05e5fc8b4a956e2f
Jodie Putrino <j.putrino@f5.com>
2017-02-01 10:49:40 -0700

    use public image from docker.io

--- scripts/docker-docs.sh
+++ scripts/docker-docs.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-exec docker run --rm -it -v $PWD:$PWD --workdir $PWD -e "LOCAL_USER_ID=$(id -u)" docker-registry.pdbld.f5net.com/tools/containthedocs "$@"
+exec docker run --rm -it -v $PWD:$PWD --workdir $PWD -e "LOCAL_USER_ID=$(id -u)" thejodesterf5/containthedocs "$@"
